62baee53204d404a5e0f93bce3ce2166e11461e0
MyCum <52251763+mycum@users.noreply.github.com>
2026-03-19 09:04:43 +0700

    Update run_check.sh

--- run_check.sh
+++ run_check.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-echo "Загрузка баз прокси..."
+echo "1. Загрузка баз прокси..."
 curl -sL "https://github.com/igareck/vpn-configs-for-russia/raw/main/BLACK_SS+All_RUS.txt" > subs.txt
 echo "" >> subs.txt
-# Используем txt-версию базы mahdibland, она идеально подходит для тестера
 curl -sL "https://raw.githubusercontent.com/mahdibland/V2RayAggregator/master/sub/sub_merge.txt" >> subs.txt
 
-echo "Установка тестера..."
-wget -q -O lite-linux-amd64 https://github.com/FacePork/LiteSpeedTest/releases/download/v0.15.0/lite-linux-amd64
+# Проверим, что ссылки скачались
+echo "Скачано строк прокси:"
+wc -l subs.txt
+
+echo "2. Установка ядра тестера (v1.0.0)..."
+# Используем вечное зеркало из репозитория mahdibland
+wget -q -O lite-linux-amd64 https://github.com/mahdibland/SSAggregator/releases/download/1.0.0/lite-linux-amd64
 chmod +x ./lite-linux-amd64
 
-echo "Старт проверки..."
+echo "3. Запуск глубокого тестирования..."
 ./lite-linux-amd64 --config lite_config.json --test subs.txt
